dab503a6050bec879528a682d2a501486147f866
zhukovvlad <zhukovvlad@yandex.ru>
2023-02-12 23:19:29 +0300

    Added User serializer in passport

--- server/services/passport.js
+++ server/services/passport.js
@@ -2,8 +2,13 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
+const { use } = require("passport");
 
 const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
 
 passport.use(
   new GoogleStrategy(
@@ -15,8 +20,13 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
+          //  we already have a user with the given profile Id
+          done(null, existingUser);
         } else {
-          new User({ googleId: profile.id }).save();
+          //  we don't have a user record, so let's make that record!
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
         }
       });
     }
