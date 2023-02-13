733866e286f4efbd0be8861eb7bf68574a65a64b
zhukovvlad <zhukovvlad@yandex.ru>
2023-02-13 23:22:16 +0300

    Initialize passport and  session

--- server/index.js
+++ server/index.js
@@ -13,11 +13,13 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
-)
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
