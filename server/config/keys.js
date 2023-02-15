e447cfd9973d3c6d92338e85546e916e545ff374
zhukovvlad <zhukovvlad@yandex.ru>
2023-02-15 21:49:00 +0300

    Added configuration kays.js with condithional environment variables choose

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+//  keys.js - figure out what credentials will return
+if (process.env.NODE_ENV === "production") {
+  //  we are in production --- return the prod set of keys
+  module.exports = require("./prod");
+} else {
+  //  we are in dev mode --- return the dev set of keys
+  module.exports = require("./dev");
+}
