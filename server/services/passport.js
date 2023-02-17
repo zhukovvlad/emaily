5a26c2f1e6163f4bd349e86c626a8e127170d31d
zhukovvlad <zhukovvlad@yandex.ru>
2023-02-17 17:42:46 +0300

    Refactored callback in passport.js. Used async/await instead of earlier used Promises

--- server/services/passport.js
+++ server/services/passport.js
@@ -24,18 +24,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          //  we already have a user with the given profile Id
-          done(null, existingUser);
-        } else {
-          //  we don't have a user record, so let's make that record!
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        //  we already have a user with the given profile Id
+        return done(null, existingUser);
+      }
+      //  we don't have a user record, so let's make that record!
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
