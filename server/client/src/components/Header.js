1462bf51545d149573fa9b170600a86f5749dc55
zhukovvlad <zhukovvlad@yandex.ru>
2023-02-22 19:05:11 +0300

    Initiated Landing page

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,26 +1,38 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return
+        return;
       case false:
         return (
-          <li><a href="/auth/google">Login With Google</a></li>
-        )
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
       default:
-        return <li><a href="/api/logout">Logout</a></li>
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
     }
   }
 
   render() {
-    console.log(this.props)
+    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul className="right">
             {/* <li>
               <a>Login With Google</a>
