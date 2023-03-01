c70f2b80ef0b902febe3e34375b776c2d42307b2
zhukovvlad <zhukovvlad@yandex.ru>
2023-03-01 21:40:02 +0300

    Added script in server/package.json for deploy client side

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
           <li key="1">
             <Payments />
           </li>,
-          <li key="3" style={{ margin: "0px 0px 0px 10px" }}>
+          <li key="3" style={{ margin: "0px 0px 0px 15px" }}>
             Credits: {this.props.auth.credits}
           </li>,
           <li key="2">
